56a82814771619066ae0db314b8dbecf0f2a2ae1
galiasmu <galiasmuzi@gmail.com>
2025-03-17 16:19:57 -0300

    creation homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
+  imports: [CommonModule]
 })
 export class HomepageComponent {
   menuOpen = false;
