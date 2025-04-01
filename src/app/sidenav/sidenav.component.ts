3b0235910620653636471bb24c841dd602198b2a
galiasmu <galiasmuzi@gmail.com>
2025-04-01 13:33:16 -0300

    bug fixes, and creation of shop

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,46 +1,27 @@
-import {MediaMatcher} from '@angular/cdk/layout';
-import {Component, OnDestroy, inject, signal} from '@angular/core';
-import {MatListModule} from '@angular/material/list';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-/** @title Responsive sidenav */
 @Component({
   selector: 'app-sidenav',
-  templateUrl: 'sidenav.component.html',
-  styleUrl: 'sidenav.component.css',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss'],
+  imports: [CommonModule],
 })
-export class SidenavComponent implements OnDestroy {
-  protected readonly fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+export class SidenavComponent {
+  activeMenu: number | null = null;
+  mobileMenuOpen: boolean = false;
 
-  protected readonly fillerContent = Array.from(
-    {length: 50},
-    () =>
-      'shop'
-  );
-
-  protected readonly isMobile = signal(true);
-
-  private readonly _mobileQuery: MediaQueryList;
-  private readonly _mobileQueryListener: () => void;
-
-  constructor() {
-    const media = inject(MediaMatcher);
-
-    this._mobileQuery = media.matchMedia('(max-width: 600px)');
-    this.isMobile.set(this._mobileQuery.matches);
-    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
-    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+  openMenu(menuId: number) {
+    this.activeMenu = menuId;
   }
 
-  ngOnDestroy(): void {
-    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  closeMenu() {
+    this.activeMenu = null;
   }
-
-  protected readonly shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(
-    window.location.host,
-  );
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+  onSearchClick() {
+    console.log('Search clicked');
+  }
 }
